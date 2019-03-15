ca0e4dff13070cb3439835c24b8859e42ef9a414
Yasuyuki Abe <yasu@yasuabe.net>
2019-03-16 06:03:11 +0900

    simplify client

--- modules/simplest/client/src/main/scala/mu_example/simplest/client/ClientApp.scala
+++ modules/simplest/client/src/main/scala/mu_example/simplest/client/ClientApp.scala
@@ -1,7 +1,7 @@
 package mu_example.simplest.client
 
 import cats.effect._
-import cats.{ApplicativeError, Functor}
+import cats.ApplicativeError
 import cats.effect.{Async, ConcurrentEffect, Resource}
 import cats.syntax.apply._
 import cats.syntax.functor._
@@ -12,11 +12,8 @@ import higherkindness.mu.rpc.config.channel.ConfigForAddress
 import monix.eval.{Task, TaskApp}
 import scala.concurrent.ExecutionContext
 
-trait SimplestClient[F[_]] {
-  def greet(name: String): F[Unit]
-}
-case class SimplestClientHandler[F[_]: ConcurrentEffect](
-    client: Resource[F, SimplestService[F]]) extends SimplestClient[F] {
+case class SimplestClient[F[_]: ConcurrentEffect](
+    client: Resource[F, SimplestService[F]]) {
 
   def greet(name: String): F[Unit] =
     Async[F].delay(println(s"greeting to $name")) *>
@@ -28,21 +25,21 @@ case class SimplestClientHandler[F[_]: ConcurrentEffect](
               ApplicativeError[F, Throwable].raiseError(e)
         }
 }
-object SimplestClientHandler {
-  def create[F[_]: Functor: ConcurrentEffect](implicit ec: ExecutionContext): F[SimplestClient[F]] =
+object SimplestClient {
+  def apply[F[_]: ConcurrentEffect](implicit ec: ExecutionContext): F[SimplestClient[F]] =
     ConfigForAddress[F]("rpc.client.host", "rpc.client.port") map { channel =>
-      SimplestClientHandler[F](SimplestService.client[F](channel))
+      SimplestClient[F](SimplestService.client[F](channel))
     }
 }
 object ClientApp extends TaskApp {
   implicit val ec: ExecutionContext = scheduler
-  
+
   private def printIO(line: String) = Task {
     println(s"${ Thread.currentThread().getId }: $line")
   }
   def run(args: List[String]): Task[ExitCode] = for {
     _      <- printIO(s"Starting client, interpreting to Future ...")
-    client <- SimplestClientHandler.create[Task]
+    client <- SimplestClient[Task]
     _      <- client.greet("World")
     _      <- printIO(s"Finishing program interpretation ...")
   } yield ExitCode.Success
